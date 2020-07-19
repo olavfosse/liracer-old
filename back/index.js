2012d586a69c18b41aaad65688872784289c4223
Olav Fosse <fosseolav@gmail.com>
2020-07-19 22:22:03 +0200

    Set up CORS

--- back/index.js
+++ back/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
-const gamesRouter = require('./controllers/games')
 
+const cors = require('cors')
+app.use(cors())
+
+const gamesRouter = require('./controllers/games')
 app.use('/api/games', gamesRouter)
+
 app.listen(process.env.HTTP_PORT, () => {
   console.log(`HTTP server running on port ${process.env.HTTP_PORT}`)
 })
