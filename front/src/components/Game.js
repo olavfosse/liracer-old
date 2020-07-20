82d90f623c8327b4fd49f1fe20f714d5c6fe2c36
Olav Fosse <fosseolav@gmail.com>
2020-07-20 02:20:46 +0200

    Add message form

--- front/src/components/Game.js
+++ front/src/components/Game.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import './Game.css'
 
-const Game = ({ quote, messages }) => (
-  <div id="game">
-    <div id="chat-header"/>
-    <div id="chat-body">
-      {
-        messages.map(({sender, content}) => (
-          <div class="message">
-            <b>&lt;{sender}&gt;</b>
-            <span>{content}</span>
-          </div>
-        ))
-      }
-    </div>
-    <div id="code-field-header"/>
-    <pre id="code-field-body">
-      {quote.code}
-    </pre>
-  </div> 
-)
+const Game = ({ quote, messages }) => {
+  return (
+    <div id="game">
+      <div id="chat-header"/>
+      <div id="chat-body">
+        {
+          messages.map(({sender, content}) => (
+            <div class="message">
+              <b>&lt;{sender}&gt;</b>
+              <span>{content}</span>
+            </div>
+          ))
+        }
+      </div>
+      <form id="chat-input">
+        <input type="text"/>
+      </form>
+      <div id="code-field-header"/>
+      <pre id="code-field-body">
+        {quote.code}
+      </pre>
+    </div> 
+  )
+}
 
 export default Game
