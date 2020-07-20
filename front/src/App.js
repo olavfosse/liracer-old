77d6cb369e7d7d4869c81befb0dc75618b565348
Olav Fosse <fosseolav@gmail.com>
2020-07-20 06:40:02 +0200

    Connect to WebSocket API

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Game from './components/Game'
 import './App.css'
 
--- front/src/components/Game.js
+++ front/src/components/Game.js
@@ -1,17 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Game.css'
+
+const wsUrl = 'ws://localhost:3001/'
 
 const Game = () => {
   const [messages, setMessages] = useState([])
   const [code, setCode] = useState('')
+  const [ws, setWs] = useState(null)
+
+  useEffect(() => {
+    const fetchWs = async () => {
+      const websocket = new WebSocket(wsUrl)
+      setWs(websocket)
+    }
+    fetchWs()
+  }, [])
+
+  useEffect(() => {
+    if(ws === null){
+      return
+    }
+
+    const saveMessage = (message) => setMessages((messages) => messages.concat(message))
+
+    const dispatch = (message) => {
+      const { type, body } = JSON.parse(message.data)
+      switch(type){
+        case('message'):
+          saveMessage(body)
+          break
+        default:
+          throw new Error('Could not dispatch message')
+      }
+    }
+
+    ws.onmessage = dispatch
+  }, [ws])
 
   return (
     <div id="game">
       <div id="chat-header"/>
       <div id="chat-body">
         {
-          messages.map(({sender, content}) => (
-            <div class="message">
+          messages.map(({sender, content}, index) => (
+            <div key={index}>
               <b>&lt;{sender}&gt;</b>
               <span>{content}</span>
             </div>
