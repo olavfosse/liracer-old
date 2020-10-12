c9612d81a0e0afa1ac5ab3d1d6431b5930aeec48
GMCgit <goranivancic117@gmail.com>
2020-10-12 21:53:09 +0200

    Updated tabs

--- models/quotes/javascript/selectionSort.js
+++ models/quotes/javascript/selectionSort.js
@@ -1,18 +1,18 @@
 function selectionSort(arr) {
-  const swap = (arr, index1, index2) =>
-    ([arr[index1], arr[index2]] = [arr[index2], arr[index1]]);
+	const swap = (arr, index1, index2) =>
+	  ([arr[index1], arr[index2]] = [arr[index2], arr[index1]]);
+	
+	for (let i = 0; i < arr.length; i++) {
+		let lowest = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[lowest] > arr[j]) {
+				lowest = j;
+			}
+		}
+		if (i !== lowest) swap(arr, i, lowest);
+	}
 
-  for (let i = 0; i < arr.length; i++) {
-    let lowest = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[lowest] > arr[j]) {
-        lowest = j;
-      }
-    }
-    if (i !== lowest) swap(arr, i, lowest);
-  }
-
-  return arr;
+	return arr;
 }
 
 const myArray = [0,4,22,45,12,15,17]
